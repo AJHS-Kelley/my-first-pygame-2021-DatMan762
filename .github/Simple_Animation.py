a20545c20251caef9f5c0d347bb20d77e3d238df
Bernard.S <90413119+DatMan762@users.noreply.github.com>
2022-01-24 14:07:55 -0500

    0v.1
    Added New Code.

--- .github/Simple_Animation.py
+++ .github/Simple_Animation.py
@@ -1,1 +1,19 @@
-# Simple Animation with PyGame, Bernard Samuels, 1/24/21, 1:49PM, v0.0
+# Simple Animation with PyGame, Bernard Samuels, 1/24/21, 1:49PM, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# setup PyGame
+pygame.intit()
+
+#setup the Window
+WINDOWWIDTH = 400
+WINDOWWIDTH = 400
+windowSurface = pygame.displayset_mode((WINDOWWIDTH,WINDOWHEIGHT),0, 32)
+pygame.display.set_caption('Animatiom Example!')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
