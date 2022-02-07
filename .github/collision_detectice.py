aba47d3517b4a02f2f88c34f0af2d828524d6abf
Bernard.S <90413119+DatMan762@users.noreply.github.com>
2022-02-07 13:50:25 -0500

    v0.1
    Imported the pygame system and random modules.

--- .github/collision_detectice.py
+++ .github/collision_detectice.py
@@ -1,1 +1,8 @@
-        # PyGame Collision Detection Practice, Bernard Samuels, february 07, 2022, 1:41pm, 0v.0
+        # PyGame Collision Detection Practice, Bernard Samuels, february 07, 2022, 1:41pm, 0v.
+
+        import pygame, sys, random
+        from pygame.locals import *
+
+        # setup PyGame
+        pygame.init()
+        mainClock = pygame.time.Clock()
