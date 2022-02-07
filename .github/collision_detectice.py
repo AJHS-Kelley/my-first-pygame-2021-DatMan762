854b41955b213944ab774c7e7bb2331b4ec1a2b6
Bernard.S <90413119+DatMan762@users.noreply.github.com>
2022-02-07 14:15:24 -0500

    v0.4
    created colors, and food data structures.

--- .github/collision_detectice.py
+++ .github/collision_detectice.py
@@ -1,4 +1,4 @@
-        # PyGame Collision Detection Practice, Bernard Samuels, february 07, 2022, 1:41pm, 0v.3
+        # PyGame Collision Detection Practice, Bernard Samuels, february 07, 2022, 1:41pm, 0v.4
 
         import pygame, sys, random
         from pygame.locals import *
@@ -18,4 +18,12 @@
         Green = (0,255,0)
         White = (255,255,255)
 
-        
+# setup the player and food data structures.
+NEWFOOD = 40
+FOODSIZE = 20
+player = pygame. Rect(300, 100, 50, 50)
+foods = []
+
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
